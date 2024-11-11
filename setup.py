3eab894ec6306f3353b7efee67a61fda221f22bd
Andrpe Baceti <a.baceti@murabei.com>
2024-11-11 14:30:51 -0300

    Building a new version 0.26.1

--- setup.py
+++ setup.py
@@ -1,30 +1,16 @@
 """setup."""
 import os
 import setuptools
-try:  # for pip >= 10
-    from pip._internal.req import parse_requirements
-except ImportError:  # for pip <= 9.0.3
-    from pip.req import parse_requirements
-
 
 with open(os.path.join(os.path.dirname(__file__), 'README.md')) as readme:
     README = readme.read()
-
-requirements_path = os.path.join(
-    os.path.dirname(__file__), 'requirements.txt')
-install_reqs = parse_requirements(requirements_path, session=False)
-try:
-    requirements = [str(ir.req) for ir in install_reqs]
-except Exception:
-    requirements = [str(ir.requirement) for ir in install_reqs]
-
 
 # allow setup.py to be run from any path
 os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
 setuptools.setup(
     name='pumpwood-miscellaneous',
-    version='0.26.0',
+    version='0.26.1',
     include_package_data=True,
     license='BSD-3-Clause License',
     description='Miscellaneous class and funcitions used in Pumpwood.',
--- setup_template.py
+++ setup_template.py
@@ -1,23 +1,9 @@
 """setup."""
 import os
 import setuptools
-try:  # for pip >= 10
-    from pip._internal.req import parse_requirements
-except ImportError:  # for pip <= 9.0.3
-    from pip.req import parse_requirements
-
 
 with open(os.path.join(os.path.dirname(__file__), 'README.md')) as readme:
     README = readme.read()
-
-requirements_path = os.path.join(
-    os.path.dirname(__file__), 'requirements.txt')
-install_reqs = parse_requirements(requirements_path, session=False)
-try:
-    requirements = [str(ir.req) for ir in install_reqs]
-except Exception:
-    requirements = [str(ir.requirement) for ir in install_reqs]
-
 
 # allow setup.py to be run from any path
 os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
