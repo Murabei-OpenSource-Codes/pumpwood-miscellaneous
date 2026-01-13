e60a932b9ba66c26cd6041420c97784316123584
André Andrade Baceti <a.baceti@murabei.com>
2026-01-13 19:28:33 -0300

    Building a new version 1.1.3

--- build/lib/pumpwood_miscellaneous/storage_connectors/azure.py
+++ build/lib/pumpwood_miscellaneous/storage_connectors/azure.py
@@ -2,20 +2,19 @@
 import os
 import io
 from typing import Callable
-from azure.storage.blob import BlobServiceClient, BlobClient, ContainerClient
-from ._general import (
-    FlaskStreamUploadWrapper, FlaskStreamDownloadWrapper)
+from azure.storage.blob import BlobServiceClient, BlobClient
+from ._general import FlaskStreamUploadWrapper
 
 
 class PumpWoodAzureStorage():
     """Class to make communication with Azure Blob Storage."""
 
     def __init__(self, bucket_name: str):
-        """
-        __init__.
+        """__init__.
 
         Args:
-            bucket_name (str): Name of the bucket.
+            bucket_name (str):
+                Name of the bucket.
         """
         # Collection AZURE_STORAGE_CONNECTION_STRING from environment
         # variables
@@ -30,25 +29,26 @@ class PumpWoodAzureStorage():
         self._bucket_name = bucket_name
 
     def check_file_exists(self, file_path: str) -> bool:
-        """
-        Check if file exists.
+        """Check if file exists.
 
         Args:
-            file_path [str]: Path to file in storage.
+            file_path (str):
+                Path to file in storage.
 
-        Return:
+        Returns:
             Return a boolean value checking if the file exists on storage.
         """
         blob = self._client.get_blob_client(blob=file_path)
         return blob.exists()
 
     def list_files(self, path: str = "") -> list:
-        """
-        List file at storage path.
+        """List file at storage path.
 
         Args:
-            path [str]: Path of the storage to list files.
-        Return [list]:
+            path (str):
+                Path of the storage to list files.
+
+        Returns:
             List of all files under path (sub-folders).
         """
         return [
@@ -57,18 +57,21 @@ class PumpWoodAzureStorage():
 
     def write_file(self, file_path: str, data: bytes, if_exists: str = 'fail',
                    content_type='text/plain') -> str:
-        """
-        Write file on Azure.
+        """Write file on Azure.
 
         Args:
-            file_path (str): Path to save the file.
-            data (str): File content in bytes.
-            if_exists (str): if_exists must be in 'overwrite',
+            file_path (str):
+                Path to save the file.
+            data (str):
+                File content in bytes.
+            if_exists (str):
+                If_exists must be in 'overwrite',
                 'overwrite_streaming' (stream file to overwrite if exists),
                 'append_breakline' (append content with a breakline between),
                 'append' (append content without break line),
                 'fail' (fail if file exists)]
-            content_type (str): Mime-type of the content.
+            content_type (str):
+                Mime-type of the content.
         """
         if_exists_opt = [
             'overwrite', 'overwrite_streaming', 'append_breakline',
@@ -95,15 +98,16 @@ class PumpWoodAzureStorage():
         return file_path
 
     def write_file_stream(self, file_path: str, data_stream: io.BytesIO):
-        """
-        Write file as stream to google cloud.
+        """Write file as stream to google cloud.
 
         Args:
             file_path (str): Path to save the stream in Google Storage Bucket.
             data_stream (io.BytesIO): Data stream.
-        Return (dict):
+
+        Returns:
             Return the file path used to save data ("file_path" key) and the
             total of bytes that were transmited.
+
         Raises:
             No particular raises at this function.
         """
@@ -122,15 +126,14 @@ class PumpWoodAzureStorage():
         return {
             "file_path": file_path, "bytes_uploaded": properties['size']}
 
-    def get_read_file_iterator(self,  file_path: str,
+    def get_read_file_iterator(self, file_path: str,
                                chunk_size: int = 1024 * 1024) -> Callable:
-        """
-        Return an iterator to stream download data in flask.
+        """Return an iterator to stream download data in flask.
 
         Args:
             file_path (str): Storage path.
-        Kwargs:
             chunk_size (int): Chunk size in bytes, default to 1Mb.
+
         Raises:
             No specific raises.
         """
@@ -139,6 +142,12 @@ class PumpWoodAzureStorage():
         return download_blob.chunks()
 
     def delete_file(self, file_path: str):
+        """Delete file from storage.
+
+        Args:
+            file_path (str):
+
+        """
         blob = self._client.get_blob_client(blob=file_path)
         if not blob.exists():
             Exception('file_path %s does not exist' % file_path)
@@ -148,7 +157,17 @@ class PumpWoodAzureStorage():
             raise Exception("Blob was not deleted from Azure")
         return True
 
-    def read_file(self, file_path: str):
+    def read_file(self, file_path: str) -> dict:
+        """Read file syncronus from storage.
+
+        Args:
+            file_path (str):
+                File path that will be read from storage.
+
+        Returns:
+            Returns a dictionary with data containing the file
+            content and content_type retrieved from storage.
+        """
         blob = self._client.get_blob_client(blob=file_path)
         if not blob.exists():
             Exception('file_path %s does not exist' % file_path)
@@ -158,15 +177,15 @@ class PumpWoodAzureStorage():
         content_type = properties["content_settings"]["content_type"]
         return {'data': data, 'content_type': content_type}
 
-    def download_to_file(self, file_path: str, file_obj: any):
-        """
-        Download file from storage and save it in a local path.
+    def download_to_file(self, file_path: str, file_obj: any) -> bool:
+        """Download file from storage and save it in a local path.
 
         Args:
-            file_obj (any): A file like object.
-            local_path (str): Local path to save file.
-        Kwargs:
-            No Kwargs
+            file_obj (any):
+                A file like object.
+            file_path (str):
+                Local path to save file.
+
         Raises:
             No specific raises.
         """
@@ -174,17 +193,18 @@ class PumpWoodAzureStorage():
         download_blob = blob.download_blob()
         download_blob.download_to_stream(file_obj)
         file_obj.close()
+        return True
 
     def get_file_hash(self, file_path: str):
-        """
-        Return file hash calculated at cloud storage provider.
+        """Return file hash calculated at cloud storage provider.
 
         Args:
-            file_path (str): File path.
-        Kwargs:
-            No Kwargs.
+            file_path (str):
+                File path at storage.
+
         Returns:
             str: Hash of the file.
+
         Raises:
             Exception("file_path {file_path} does not exist")
                 If file is not found on storage.
@@ -196,13 +216,17 @@ class AzureStorageUploadFileStream:
     """Create a upload file stream for Google Storage."""
 
     def __init__(self, blob: BlobClient, data_stream: io.BytesIO, **kwargs):
-        """
-        __init__.
+        """__init__.
 
         Args:
-            blob [BlobClient]:
-            data_stream [io.BytesIO]:
-        Return:
+            blob (BlobClient):
+                Azure blob storage client
+            data_stream (io.BytesIO):
+                Data strem as a BytesIO
+            **kwargs:
+                Other arguments.
+
+        Returns:
             Azure Storage Upload FileStream
         """
         self._blob = blob
