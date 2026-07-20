43e40fce9580a9d71e540fbd6d96a2b8e3857eb3
Lucas Ribeiro Magalhaes <lucas.magalhaes@murabei.com>
2026-07-20 15:23:30 -0300

    Building a new version 1.1.6

--- build/lib/pumpwood_miscellaneous/storage_connectors/__init__.py
+++
@@ -1,8 +0,0 @@
-"""Module to make a standard to connect with different storages."""
-from .aws import PumpWoodAwsS3
-from .azure import PumpWoodAzureStorage
-from .google import PumpWoodGoogleBucket
-
-__all__ = [
-    PumpWoodAwsS3, PumpWoodAzureStorage, PumpWoodGoogleBucket,
-]
